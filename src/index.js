6017c9b2881302acbfc05d7ea4755f419263564f
Maria <sheyko.mari@mail.ru>
2021-03-11 00:12:09 +0300

    fix: change task code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 
 exports.min = function min (arr) {
-  if (arr?.length) {
-     arr.sort( ((a,b) => {return a-b}))
-    return arr[0];
-  } else return 0;
+  if (!arr || !arr.length) return 0;
+  arr.sort( ((a,b) => {return a-b}))
+  return arr[0];
 }
 
 exports.max = function max (arr) {
-  if (arr?.length){
-     arr.sort( ((a,b) => {return a-b}))
-    return arr[arr.length-1];
-  } else return 0;
+  if (!arr || !arr.length) return 0;
+  arr.sort( ((a,b) => {return a-b}))
+  return arr[arr.length-1];
 }
 
 exports.avg = function avg (arr) {
-  if (arr?.length) {
-    let result = 0;
-    for (let i = 0; i < arr.length; i++) {
-      result += arr[i];
-    }
-    return result/arr.length;
-  } else return 0;
+  if (!arr || !arr.length) return 0;
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  return result/arr.length;
 }
