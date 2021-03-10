5fdef8ca2b96b2f4ff293b32af753265f93a7933
Maria <sheyko.mari@mail.ru>
2021-03-11 00:04:01 +0300

    feat: resolve tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if (arr?.length) {
+     arr.sort( ((a,b) => {return a-b}))
+    return arr[0];
+  } else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if (arr?.length){
+     arr.sort( ((a,b) => {return a-b}))
+    return arr[arr.length-1];
+  } else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if (arr?.length) {
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
+      result += arr[i];
+    }
+    return result/arr.length;
+  } else return 0;
 }
